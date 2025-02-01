327435c41539a2d050aa320fce9d5917522f6f11
Wiz Abhi <abhishekg8318@gmail.com>
2025-02-01 10:38:22 +0530

    change

--- next.config.mjs
+++ next.config.mjs
@@ -1,9 +1,7 @@
-// next.config.js
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   experimental: {
-    webpackBuildWorker: false,
-    parallelServerCompiles: false
+    webpackBuildWorker: false
   }
 }
 
